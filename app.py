0f4ea0842efc2e9bf3e84a59cba86040205002c0
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-12-17 16:02:15 +0100

    working state

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import requests
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def get_data():
-    print(requests.get('https://randomuser.me/api/ ').content)
-    return requests.get('https://randomuser.me/api/ ').content
+    print(requests.get('https://randomuser.me/api/').content)
+    return requests.get('https://randomuser.me/api/').content
+
+
 
 if __name__ == '__main__':
     app.run()
