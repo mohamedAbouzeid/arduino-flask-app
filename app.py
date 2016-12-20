2a066a47a3874968fd5e850f06b830039bc723bd
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-12-20 12:47:00 +0100

    working state
    working state

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import requests
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+print(os.environ['DATABASE_URL'])
+db = SQLAlchemy(app)
 
 @app.route('/')
 def get_data():
