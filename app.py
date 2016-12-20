1893cd63f7a7b3a84709021a52ad32753491c473
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-12-20 14:49:19 +0100

    working state

--- app.py
+++ app.py
@@ -11,25 +11,24 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(200))
     title = db.Column(db.String(100))
-    picture_url =  db.Column(db.String(200))
+    picture_url = db.Column(db.String(200))
 
     def __init__(self, url, title, picture_url):
         self.url = url
         self.title = title
         self.picture_url = picture_url
 
-    def __repr__(self):
-        return '<Title %r>' % self.title
-
-@app.route('/')
-def add_data():
-    article = Article('google.com', 'john.doe@example.com', 'google-image.com')
-    db.session.add(article)
-    db.session.commit()
-    print(requests.get('https://randomuser.me/api/').content)
-    return requests.get('https://randomuser.me/api/').content
+    # def __repr__(self):
+    #     return '<Title %r>' % self.title
 
 @app.route('/app/list')
+def add_data():
+    article = Article('google.com', 'hamada', 'google-image.com')
+    db.session.add(article)
+    db.session.commit()
+    return 'added'
+
+@app.route('/')
 def get_data():
     all_users = Article.query.all()
     print(all_users)
